d47079d0e92bf2d8f6bf4e76337f47d7ad89a4e1
baosiek <baosiek@gmail.com>
2020-12-28 14:54:14 -0800

    Adjusted to also doenload Bert

--- get_aclImdb.sh
+++ get_aclImdb.sh
@@ -1,13 +1,27 @@
 #! /bin/bash
 
-# There is the need to check if /data directory exists, and create it in case it doesn't.
-dir=./data/aclImdb
+# Downloads aclImdb_v1.tar.gz
+dir=./data/
 if [[ ! -d "$dir" ]]; then
+    mkdir -p ./$dir
 	echo "Downloading aclImdb."
    	wget http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz
-    tar zxf aclImdb_v1.tar.gz --directory ./data/ 
+    tar zxf aclImdb_v1.tar.gz --directory ./$dir 
     rm -f aclImdb_v1.tar.gz
     echo "aclImdb downloaded and extracted into $dir."  
 else
     echo "aclImdb already downloaded."
+fi
+
+# Downloads Bert
+bert_dir=./bert_uncased
+if [[ ! -d "$bert_dir" ]]; then
+    mkdir -p ./$bert_dir
+	echo "Downloading bert_uncased_L-12_H-768_A-12_1."
+    wget -O bert_uncased.tar.gz https://tfhub.dev/google/bert_uncased_L-12_H-768_A-12/1?tf-hub-format=compressed
+    tar zxf bert_uncased.tar.gz --directory ./$bert_dir 
+    rm -f bert_uncased.tar.gz
+    echo "Bert Uncased downloaded and extracted into $bert_dir."  
+else
+    echo "Bert Uncased already downloaded."
 fi
