4cbbd2a19a6c54bcb65720f42b817ef27459b0a6
baosiek <baosiek@gmail.com>
2020-12-28 12:45:25 -0800

    BERT with TF 1.15 implemented

--- get_aclImdb.sh
+++ get_aclImdb.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
+
+# There is the need to check if /data directory exists, and create it in case it doesn't.
 dir=./data/aclImdb
 if [[ ! -d "$dir" ]]; then
 	echo "Downloading aclImdb."
    	wget http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz
-    	tar zxf aclImdb_v1.tar.gz --directory ./data/ 
-    	rm -f aclImdb_v1.tar.gz
-    	echo "aclImdb downloaded and extracted into $dir."  
+    tar zxf aclImdb_v1.tar.gz --directory ./data/ 
+    rm -f aclImdb_v1.tar.gz
+    echo "aclImdb downloaded and extracted into $dir."  
 else
-    	echo "aclImdb already downloaded."
+    echo "aclImdb already downloaded."
 fi
