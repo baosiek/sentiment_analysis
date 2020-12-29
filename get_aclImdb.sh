971ead2b81b5d5096f0e63148ebf5ea15d62c5a2
baosiek <baosiek@gmail.com>
2020-12-28 18:53:27 -0800

    get_aclimdb.sh directory structure

--- get_aclImdb.sh
+++ get_aclImdb.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
 
+# Directory vocab.txt
+model_dir=./model/bert/assets
+if [[ ! -d "$model_dir" ]]; then
+    mkdir -p ./$model_dir
+	echo "$model_dir created!."  
+else
+    echo "$model_dir already exists."
+fi
+
 # Downloads aclImdb_v1.tar.gz
 dir=./data/
 if [[ ! -d "$dir" ]]; then
@@ -21,6 +30,7 @@ if [[ ! -d "$bert_dir" ]]; then
     wget -O bert_uncased.tar.gz https://tfhub.dev/google/bert_uncased_L-12_H-768_A-12/1?tf-hub-format=compressed
     tar zxf bert_uncased.tar.gz --directory ./$bert_dir 
     rm -f bert_uncased.tar.gz
+    cp $bert_dir/assets/vocab.txt $model_dir/vocab.txt
     echo "Bert Uncased downloaded and extracted into $bert_dir."  
 else
     echo "Bert Uncased already downloaded."
